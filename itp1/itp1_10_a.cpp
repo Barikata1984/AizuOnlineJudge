f7728d9da9daf17110b54edd5590934b3cad6faf
Atsushi Kuno <franche1984@gmail.com>
2019-12-26 23:51:57 +0900

    added some codes

---
+++ itp1/itp1_10_a.cpp
@@ -0,0 +1,1 @@
+#include <iomanip>#include <iostream>#include <cmath>int main(){    double x1, y1, x2, y2;    std::cin >> x1 >> y1 >> x2 >> y2;        std::cout << std::fixed << std::setprecision(4) << std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2)) << std::endl;    return 0;}
