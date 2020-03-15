86af89fa537cf9ba4d38dac695b20acaf956b70e
Atsushi Kuno <franche1984@gmail.com>
2020-03-15 23:56:08 +0900

    added some codes

--- dpl/dpl_1_c.cpp
+++ dpl/dpl_1_c.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
 
-#define INF 1000000007
-
 int main(){
     int n, limit;
     std::cin >> n >> limit;
@@ -12,19 +10,15 @@ int main(){
         std::cin >> value.at(i) >> weight.at(i);
     }
 
-    std::vector<std::vector<std::vector<int>>> dp(n + 1, std::vector<std::vector<int>>(limit + 1, std::vector<int>(10001, INF)));
-    for(int i = 0; i <= n; ++i){
-        dp.at(i).at(0).at(0);
-    }
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(limit + 1, 0));
 
     for(int i = 0; i < n; ++i){
         for(int w = 0; w <= limit; ++w){
-            for(int w = 0; w <= limit; ++w){
-                if(weight.at(i) <= w){
-                    dp.at(i + 1).at(w) += std::max(dp.at(i).at(w - weight.at(i)) + value.at(i), dp.at(i).at(w));
-                }else{
-                    dp.at(i + 1).at(w) = dp.at(i).at(w);
-                }
+            if(weight.at(i) <= w){
+                dp.at(i + 1).at(w) += std::max(dp.at(i).at(w - weight.at(i)) + value.at(i), dp.at(i).at(w));
+            }else{
+                dp.at(i + 1).at(w) = dp.at(i).at(w);
+            }
         }
     }
 
