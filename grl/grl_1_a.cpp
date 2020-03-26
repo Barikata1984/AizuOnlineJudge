621bed60a2fdb2e96fa2d9890449c001d04529b1
Atsushi Kuno <franche1984@gmail.com>
2020-03-26 23:54:56 +0900

    added a code

--- grl/grl_1_a.cpp
+++ grl/grl_1_a.cpp
@@ -1,56 +1,66 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <queue>
+#include <unordered_map>
 
-#define INF 1000000007
+using Graph = std::vector<std::unordered_map<long long, long long>>;
+using PairLL = std::pair<long long, long long>;
 
-using Graph = std::vector<std::vector<std::pair<int, int>>>;
+#define INF (long long)(1e18 + 7)
 
-void print(std::vector<long long> &vec){
+void print(std::vector<long long> vec){
     int n = vec.size();
     for(int i = 0; i < n; ++i){
-        if(INF == vec.at(i)){
+        if((long long)5e10 < vec.at(i)){
             std::cout << "INF" << std::endl;
         }else{
             std::cout << vec.at(i) << std::endl;
-        }
-    }
-}
-
-// depth first search
-void dfs(std::vector<long long> &w, const Graph &g, const int r){
-    for(auto elem : g.at(r)){
-        int first = elem.first, second = elem.second;
-        w.at(first) = std::min(w.at(first), w.at(r) + second);
-        dfs(w, g, first);
-
-//        auto itr = std::find(g.at(r).begin(), g.at(r).end(), elem);
-//        if(g.at(r).end() != itr){
-//            g.at(r).erase(std::find(g.at(r).begin(), g.at(r).end(), elem));
-//        }
+        } 
     }
 }
 
 int main(){
-    int v, e, r;
+    long long v, e, r;
     std::cin >> v >> e >> r;
-    
-    if(1 == v){
-        std::cout << 0 << std::endl;
-        return 0;
-    }
 
     Graph g(v);
-    int s, t, d;
+    long long s, t, d;
     while(std::cin >> s >> t >> d){
-        g.at(s).push_back(std::make_pair(t, d));
-//        g.at(t).push_back(std::make_pair(s, d)); // a directed graph is used in this exercise
+        g.at(s).insert(std::make_pair(t, d));
     }
 
-    std::vector<long long> w(v, INF);
-    w.at(r) = 0;
-    dfs(w, g, r);
-    print(w);
+    auto compare = [](PairLL &lhs, PairLL &rhs){
+        return lhs.second < rhs.second;
+    }; 
+   
+    std::priority_queue<
+        long long,
+        std::vector<PairLL>,
+        decltype(compare) 
+    > pq(compare);
+    pq.push(std::make_pair(r, 0));
+
+    std::vector<long long> dist(v, INF);
+    dist.at(r) = 0;
+
+    while(!pq.empty()){
+        auto from = pq.top();
+        pq.pop();
+        
+        if(dist.at(from.first) < from.second){
+            continue;
+        }
+
+        for(auto to : g.at(from.first)){
+            auto len = dist.at(from.first) + to.second;
+            if(len < dist.at(to.first)){
+                dist.at(to.first) = len;
+                pq.push(std::make_pair(to.first, dist.at(to.first)));
+            }
+        }
+    }
+
+    print(dist);
+
     return 0;
 }
